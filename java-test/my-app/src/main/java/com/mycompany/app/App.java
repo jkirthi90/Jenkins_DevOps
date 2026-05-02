67bfd8a9bd3e5c5a4329bf43bf939b3c9893278f
jkirthi90 <jkirthi90@gmail.com>
2026-05-02 17:15:23 +0530

    Update App.java

--- java-test/my-app/src/main/java/com/mycompany/app/App.java
+++ java-test/my-app/src/main/java/com/mycompany/app/App.java
@@ -3,7 +3,7 @@ package com.mycompany.app;
 public class App {
 
     public int add(int a, int b) {
-        return a + b;
+        return a + b + 0;
     }
 
     public int subtract(int a, int b) {
